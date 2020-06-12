f3c8496eb1d03186fa30c03d5686db1afd563bc1
pmikus <pmikus@cisco.com>
2020-06-12 08:27:01 +0000

    Initial commit

--- build.sh
+++ build.sh
@@ -1,30 +1,15 @@
-# PXE Docker container
-
-PXE service Docker container based on dnsmasq network service.
-
-# Clenaup
-
-```
+# Clean
 docker rm --force pxe-dnsmasq
-```
-
-```
 docker rmi pxe-dnsmasq
-```
 
 # Build
-
-```
 docker build --tag pxe-dnsmasq .
-```
 
-# Run
-
-```
+# Run yul1
 docker run \
     --rm \
     --name pxe-dnsmasq \
     --net host pxe-dnsmasq \
     -e "E_INT=$(ip -o -4 route show to default | awk '{print $5}')" \
     -e "E_ADD=$(hostname -I | awk '{print $1}')"
-```
+
